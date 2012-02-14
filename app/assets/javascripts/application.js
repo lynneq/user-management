d2caf956ce2019e045dac01588b470c5e0e46c1f
Lina Qu <lqu@thoughtworks.com>
2012-02-14 12:11:59 -0600

    03 User: JQuery validation, email format.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,29 +21,37 @@ $(document).ready(function(){
 		var user_name = $("#user_name").val();
 		var user_password = $("#user_password").val();
 		var user_password_confirmation = $("#user_password_confirmation").val();
-		var user_email = $("#user_name").val();
+		var user_email = $("#user_email").val();
 		
 		if( user_name == '') {
 			error_msg +="Username is empty <br>";
 		};
-		if(user_password == '') {
+		if( user_password == '') {
 			error_msg +="Password is empty <br>";
 		};
-		if(user_password_confirmation == '') {
+		if( user_password_confirmation == '') {
 			error_msg +="Password confirmation is empty <br>";
 		};
-		if(user_password_confirmation != user_password) {
+		if( user_password_confirmation != user_password) {
 			error_msg +="Password confirmation doesn't match Password <br>";
 		};	
 		if( user_email == '') {
 			error_msg +="Email is empty <br>";
 		};
-
+		var emailRegExp = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+		if( !emailRegExp.test(user_email) ){
+			error_msg += "Email format is invalid <br>";
+	 	};
+			
 		if(error_msg!=""){
 			// alert(error_msg);
-			$('#new_user').append('<div id="errorMessage" class="message">');
-			$('#new_user').append(error_msg);
-			$('#new_user').append('</div>');
+			$('#errorMessage').detach()
+			
+			errorMessageDiv = $('<div id="errorMessage"/>');
+			errorMessageDiv.append(error_msg);
+			
+			$('#new_user').append(errorMessageDiv);
+
 			return false;
 		}
 	})
