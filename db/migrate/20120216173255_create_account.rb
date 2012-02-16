0dc6d2cc26466337b580d05ed7d110d3d159016a
Lina Qu <lqu@thoughtworks.com>
2012-02-16 14:29:08 -0600

    12 Account migration.

---
+++ db/migrate/20120216173255_create_account.rb
@@ -0,0 +1,16 @@
+class CreateAccount < ActiveRecord::Migration
+  def up
+    create_table :accounts do |t|
+      t.string :name
+      t.time :member_since
+      t.string :level
+      t.text :description
+      
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :accounts
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120213201932) do
+ActiveRecord::Schema.define(:version => 20120216173255) do
+
+  create_table "accounts", :force => true do |t|
+    t.string   "name"
+    t.time     "member_since"
+    t.string   "level"
+    t.text     "description"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
